778eedcb990cf217417df86c9142901348ba94d0
Dmitry Grinshteyn <dgrin2005@mail.ru>
2019-07-07 19:21:21 +0200

    Added stripHtml extention

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -9,3 +9,74 @@ fun String.truncate(value: Int = 16): String {
         return substring + fill
     }
 }
+
+fun String.stripHtml():String?{
+    var newString = ""
+    var i = 0
+    while (i < this.length) {
+        when (this[i]) {
+            '<' -> {
+                var newString2 = ""
+                var j = true
+                while (j) {
+                    j = i < this.length
+                    if (j) {
+                        if (this[i] != '>') {
+                            newString2 += this[i]
+                        } else {
+                            j = false
+                        }
+                    }
+                    i++
+                }
+                if (i > this.length) {
+                    newString += "<" + newString2.substring(1, newString2.length).stripHtml()
+                }
+            }
+            '&' -> {
+                var newString2 = ""
+                var j = true
+                while (j) {
+                    j = i < this.length
+                    if (j) {
+                        if (this[i] != ';') {
+                            newString2 += this[i]
+                        } else {
+                            j = false
+                        }
+                    }
+                    i++
+                }
+                if (i > this.length) {
+                    newString += "&" + newString2.substring(1, newString2.length).stripHtml()
+                }
+            }
+            ' ' -> {
+                var j = true
+                while(j) {
+                    j = i < this.length
+                    if (j) {
+                        if (this[i] == ' ') {
+                            i++
+                        } else {
+                            j = false
+                        }
+                    }
+                }
+                newString += ' '
+            }
+            '\"' -> {
+                i++
+            }
+            '\'' -> {
+                i++
+            }
+            else -> {
+                newString += this[i]
+                i++
+            }
+        }
+
+    }
+    return newString
+}
